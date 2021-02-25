f5b0cd77c2e8c609c6143631c6750a2302586491
rabiul-hasan <rasel.ctg71@outlook.com>
2021-02-24 19:32:48 -0700

    Update onn.py

--- onn.py
+++ onn.py
@@ -212,7 +212,8 @@ class Net(torch.nn.Module):
         a, b = self.model(torch.ones((self.size, self.size, 2)), torch.zeros((self.size, self.size, 2)))
         c, d = self.model(torch.zeros((self.size, self.size, 2)), torch.ones((self.size, self.size, 2)))
 
-
+        print(a.shape, "shape of a in forward")
+        print(b.shape, "shape of b in forward")
         # a, b = self.model(torch.ones((200, 200, 2)), torch.zeros((200, 200, 2)))
         # c, d = self.model(torch.zeros((200, 200, 2, 2)), torch.ones((200, 200, 2)))
 
